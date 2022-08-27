0e3d36d740c7641e9550e0336a0b453129326769
akuma-dhruv <dhruvgarg79@gmail.com>
2022-08-27 11:40:34 +0530

    added dataset

--- sudoku3.js
+++ sudoku3.js
@@ -1,23 +1,27 @@
-var board = [
-    "--74916-5",
-    "2---6-3-9",
-    "-----7-1-",
-    "-586----4",
-    "--3----9-",
-    "--62--187",
-    "9-4-7---2",
-    "67-83----",
-    "81--45---"
-]
-
-var solution = [
-    "387491625",
-    "241568379",
-    "569327418",
-    "758619234",
-    "123784596",
-    "496253187",
-    "934176852",
-    "675832941",
-    "812945763"
-]
+async function loadArray()
+{
+    const response = await fetch('data/sudoku.json');
+    const data =await response.json();
+    return data;
+}
+async function generateMatrix()
+{
+    var num;
+    var board ="" ;
+    var solution="" ;
+    let data=[];
+    num=Math.floor(Math.random()*2243);
+    try{
+        data = await loadArray();
+        board=data[num].que;
+        solution=data[num].sol;
+    }
+    catch(err){
+        console.log("Error!");
+        console.log(err);
+    }
+    console.log(num);
+    console.log(board);
+    console.log(solution);
+}
+document.addEventListener("DOMContentLoaded",generateMatrix());
